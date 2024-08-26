8d1979bc8bc3713307cba69ba76ec8a2fe07a0ee
Olivier Bourgeois <bourgeoisolivier@hotmail.fr>
2024-08-26 15:27:54 +0200

    delete profile & user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -153,4 +153,24 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @router DELETE api/profile
+// @desc Delete profile, user & posts
+// @access Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndDelete({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndDelete({ _id: req.user.id });
+
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
